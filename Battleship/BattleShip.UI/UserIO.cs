99f82932a46284f2686c495a8289cc21b99943d6
Eric Stafford <esstafford14@gmail.com>
2019-01-27 22:01:12 -0600

    Updates

--- Battleship/BattleShip.UI/UserIO.cs
+++ Battleship/BattleShip.UI/UserIO.cs
@@ -8,7 +8,7 @@ namespace BattleShip.UI
 {
     public class UserIO
     {
-        private static void SplashScreen()
+        public static void SplashScreen()
         {
             Console.WriteLine("<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>");
             Console.WriteLine("<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>");
@@ -21,12 +21,12 @@ namespace BattleShip.UI
             Console.ReadLine();
         }
 
-        public string WelcomeUser()
+        public static void WelcomeUser()
         {
             Console.WriteLine("Welcome to Battleship");
         }
 
-        public string GetName1()
+        public string Player1()
         {
             Console.WriteLine("User 1. What is your name?");
             string name1 = Console.ReadLine();
@@ -57,14 +57,14 @@ namespace BattleShip.UI
                         continue;
                     }
                 }
-
                 for (int i = 0; i < xStr.Length; i++)
                 {
                     if (xCoord > 10)
                     {
                         continue;
                     }
-                    else {
+                    else
+                    {
                         if (coords[0] == xStr[i])
                         {
                             xCoord = i + 1;
@@ -81,65 +81,127 @@ namespace BattleShip.UI
 
                 return xy;
             }
-         }
+        }
 
-            public static void DisplayBoard(Board board)
+        public static void DisplayBoard(Board board)
+        {
+            string xStr = "ABCDEFGHIJ";
+            char row = 'Z';
+            ShotHistory displayShot = new ShotHistory();
+            Console.WriteLine("   [1  2  3  4  5  6  7  8  9  10]");
+
+            for (int x = 0; x < 11; x++)
             {
-                for (int x = 0; x < 11; x++)
-                {
+                row = xStr[x];
+                Console.Write($"\n[{row}]");
+
                 for (int y = 0; y < 11; y++)
                 {
                     if (x == 0 && y == 0)
                     {
-                        Console.Write("[ ]");
+                        Coordinate coord = new Coordinate(x, y);
+                        displayShot = board.CheckCoordinate(coord);
+                        Console.Write($"[{displayShot}]");
                     }
-
-                }
-                }
-            }
-
-            
-
-            public string Randomize()
-            {
-                Random Player = new Random();
-                int random = Player.Next(1, 2);
-                Console.WriteLine($"Player {Player} goes first.");
-                if (random == 2)
-                {
-                    //Player1Method
-                }
-                else
-                {
-                    //Player2Method
-                }
-            }
-
-            public static string VictoryAndGlory()
-            {
-                Console.WriteLine("You're a winnah, baby");
-                Console.WriteLine("\n \n");
-                Console.WriteLine("Would you like to play again?");
-                string input = Console.ReadLine().ToLower;
-
-                while (true)
-                {
-                    if (input == "yes")
-                    {
-                        return WelcomeUser();
-                    }
-                    else if (input == "no")
-                    {
-                        Console.WriteLine("Thanks for playing!");
-                        Environment.Exit(0);
-                    }
-                    else
-                    { 
-                    Console.WriteLine("Could you try that again? I didn't understand you.");
-                        continue;
-                    }
+                    // give it the board for the opponent, not displaying the current player's board
                 }
             }
         }
-    }
 
+        public string Randomize()
+        {
+            Random Player = new Random();
+            int random = Player.Next(1, 2);
+            Console.WriteLine($"Player {Player} goes first.");
+            if (random == 2)
+            {
+                //Player1Method
+            }
+            else
+            {
+                //Player2Method
+            }
+        }
+
+        public static string VictoryAndGlory()
+        {
+            Console.WriteLine("You're a winnah, baby");
+            Console.WriteLine("\n \n");
+            Console.WriteLine("Would you like to play again?");
+            string input = Console.ReadLine().ToLower;
+
+            while (true)
+            {
+                if (input == "yes")
+                {
+                    return WelcomeUser();
+                }
+                else if (input == "no")
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Could you try that again? I didn't understand you.");
+                    continue;
+                }
+            }
+        }
+
+        public static string PlacingShips()
+        {
+            while (true)
+            {
+                for (int i = 1; i < 6; i++)
+                {
+                    if (i == 1)
+                    {
+
+                       Console.WriteLine($"Place the {ShipType.Battleship}.");
+                        Console.WriteLine($"Enter coordinate: {UserIO.EnterCoordinates}");
+                        string bShip = Console.ReadLine();
+                        Console.WriteLine($"{ShipPlacement}");
+                    }
+                    else if (i == 2)
+                    {
+                        Console.WriteLine($"Place the {ShipType.Carrier}");
+                        Console.WriteLine($"Enter coordinate: {UserIO.EnterCoordinates}");
+                        string caShip = Console.ReadLine();
+                        Console.WriteLine($"{ShipPlacement}")
+                    }
+                    else if (i == 3)
+                    {
+                        Console.WriteLine($"Place the {ShipType.Cruiser}");
+                        Console.WriteLine($"Enter coordinate: {UserIO.EnterCoordinates}");
+                        string crShip = Console.ReadLine();
+                        Console.WriteLine($"{ShipPlacement}")
+                    }
+                    else if (i == 4)
+                    {
+                        Console.WriteLine($"Place the {ShipType.Destroyer}");
+                        Console.WriteLine($"Enter coordinate: {UserIO.EnterCoordinates}");
+                        string dShip = Console.ReadLine();
+                        Console.WriteLine($"{ShipPlacement}")
+                    }
+                    else if (i == 5)
+                    {
+                        Console.WriteLine($"Place the {ShipType.Submarine}");
+                        Console.WriteLine($"Enter coordinate: {UserIO.EnterCoordinates}");
+                        string subShip = Console.ReadLine();
+                        Console.WriteLine($"{ShipPlacement}")
+                    }
+                }
+                continue;
+            }
+        }
+
+        public static Coordinate FireAway()
+        {
+            ShotHistory
+            ShotStatus
+        }
+
+        public static string 
+    }
+}
--- Battleship/BattleShip.UI/Workflow.cs
+++ Battleship/BattleShip.UI/Workflow.cs
@@ -10,112 +10,92 @@ namespace BattleShip.UI
     {
         public void Run()
         {
-            Coordinate yx = UserIO.EnterCoordinates();
 
-            //        //start menu/create splashscreen
+            UserIO.SplashScreen();
+            UserIO.WelcomeUser();
+        
+            //Players and Boards
 
-            //        //start method
-            //        (UserIO.WelcomeUser)
+            Player player1 = new Player();
+            Player player2 = new Player();
+            Board board1 = new Board();
+            Board board2 = new Board();
 
-            //        //start User1Method
+            //User1SetUp
+            UserIO.PlacingShips.board1();
+            UserIO.PlacingShips(ShipType.Battleship);
+            //place ship (until all ships are printed)
+            (UserIO.EnterCoordinates)
+                        ShipDirection
+            Console.WriteLine("Press any key, and pass to User 2.");
+            Console.ReadLine();
+            Console.Clear();
 
-            //        UserIO.GetName1(name1) = new GetName1();
-            //        UserIO.GetName2(name2) = new GetName2();
-            //        Board board1 = new Board();
-            //        Board board2 = new Board();
+            //User2SetUp
+            (UserIO.GetName2)
+                (ShipPlacement.ShipPlacement)(ShipType.ShipType)(Ship.Ship)(5, 4, 3, 2, 1)
+                        UserIO.EnterCoordinates;
+            ShipDirection.ShipDirection;
+            Console.WriteLine("Press any key, and pass to User 2.");
+            Console.ReadLine();
+            Console.Clear();
 
-            //        (UserIO.GetName1)
-            //            (shipPlacement)(shipType)(ship)(5, 4, 3, 2, 1)
-            //            //place ship (until all ships are printed)
-            //            (UserIO.EnterCoordinates)
-            //            //Coordinate?
-            //            ShipDirection.cs
-            //        //What direction?
-            //        //else if (all ships have not been placed) loop back to place ship;
+            //Randomize
+            (UserIO.Randomize)
 
-            //        //assign to user1
+            //Player1GamePlay
+            Console.WriteLine("Press any key to start your turn.");
+                Console.ReadLine();
+                //playerX and boardY
+            
+                    (UserIO.EnterCoordinates)(Coordinate.Equals)(loop for ships)
+                        //ask player1 for shot coordinate (trap user in while loop until valid) (#5)
+                        (ShotStatus method) 
+                        //validate shot && check shotType 
+                        (ShotHistory method)
+                        //display hit, miss, or invalid
+                         
+                        //did player1 win? if statement (#6)
+                        //if (all 5 battleships sunk) 
+                        //go to Victory message
+                        //if (not all 5 battleships sunk)
+                    
+                
+                    Console.WriteLine("Press any key to end your turn");
+                    Console.ReadLine();
+                    Console.Clear();
 
-            //        Console.WriteLine("Press any key, and pass to User 2.");
-            //        Console.ReadLine();
-            //        Console.Clear();
-            //        //else if (all ships have been placed) (end #2)
+                
 
 
-            //        //Start User2Method
+                Player2Method;
+                  {
+                Console.WriteLine("Press any key to start your turn.");
+                Console.ReadLine();
+                //playerY and boardX
+                while (true)
+                {
+                    (UserIO.EnterCoordinates)(loop for ships)
+                        //ask player2 for shot coordinate (trap user in while loop until valid) (#8)
+                        (ShotStatus method) 
+                        //validate shot && check shotType 
+                        (ShotHistory method) = int history
+                        //display hit, miss, or invalid
 
-            //        (UserIO.GetName2)
-            //            (ShipPlacement.ShipPlacement)(ShipType.ShipType)(Ship.Ship)(5, 4, 3, 2, 1)
-            //            //place ship (1 - 5)
-            //            UserIO.EnterCoordinates;
-            //        //Coordinate?
-            //        ShipDirection.ShipDirection;
-            //        //What direction?
-            //        //else if (all ships not been placed) loop back to place ship;
+                        else
+                        {
+                    //did player2 win? if statement (#9)
+                    //if (all 5 battleships sunk)
+                    // go to Victory message
+                    //else if (not all 5 battleships sunk) 
+                }
+                Console.WriteLine("Press any key to end your turn");
+                Console.ReadLine();
+                Console.Clear();
+            }
 
-            //        //assign to user2
+            UserIO.VictoryAndGlory();
 
-            //        Console.WriteLine("Press any key, and pass to User 2.");
-            //        Console.ReadLine();
-            //        Console.Clear();
-
-            //        (UserIO.Randomize)
-
-            //        Player1Method
-            //        {
-            //            Console.WriteLine("Press any key to start your turn.");
-            //            Console.ReadLine();
-            //            //player1 and board2
-            //            while (true)
-            //            {
-            //                (UserIO.EnterCoordinats)(Coordinate.Equals)(loop for ships)
-            //                    //ask player1 for shot coordinate (trap user in while loop until valid) (#5)
-            //                    (ShotStatus method) 
-            //            //validate shot && check shotType 
-            //            (ShotHistory method)
-            //            //display hit, miss, or invalid
-            //            } 
-            //            else
-            //            {
-            //                //did player1 win? if statement (#6)
-            //                //if (all 5 battleships sunk) 
-            //                //go to Victory message
-            //                //if (not all 5 battleships sunk)
-            //                Console.WriteLine("Press any key to end your turn");
-            //                Console.ReadLine();
-            //                Console.Clear();
-            //            }
-            //        }
-
-            //        Player2Method
-            //      {
-            //            Console.WriteLine("Press any key to start your turn.");
-            //            Console.ReadLine();
-            //            //player2 and board1
-            //            while (true)
-            //            {
-            //                (UserIO.EnterCoordinates)(loop for ships)
-            //                    //ask player2 for shot coordinate (trap user in while loop until valid) (#8)
-            //                    (ShotStatus method) 
-            //            //validate shot && check shotType 
-            //            (ShotHistory method) = int history
-            //            //display hit, miss, or invalid
-            //            }
-            //            else
-            //            {
-            //                //did player2 win? if statement (#9)
-            //                //if (all 5 battleships sunk)
-            //                // go to Victory message
-            //                //else if (not all 5 battleships sunk) 
-            //            }
-            //                Console.WriteLine("Press any key to end your turn");
-            //                Console.ReadLine();
-            //                Console.Clear();
-            //            }
-
-            //        UserIO.VictoryAndGlory();
-
-            //    }
-            //}
         }
     }
 }
